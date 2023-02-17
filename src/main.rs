b355d4302c2908687aad71e6e69bf9cbac0ebd24
zCubed3 <liamr2023@gmail.com>
2023-02-17 12:44:44 -0800

    Prefab conversion (with adjacency)

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,14 @@
 mod meta_file;
 mod dropwatch;
 
-use std::fs::{File, read_dir, create_dir};
+use std::collections::HashMap;
+use std::fs::{File, read_dir, create_dir, copy, write};
 use std::path::{Path, PathBuf};
-use std::env;
+use std::{env, fs};
+use std::io::Read;
 
 use crate::dropwatch::Dropwatch;
 use crate::meta_file::*;
-
-fn collect_recurse<P: AsRef<Path>>(path: P, dirs: &mut Vec<PathBuf>) {
-    for entry_result in read_dir(path).expect("Failed to read given path!") {
-        // If we can't read a meta file we probably shouldn't be in here
-        let entry = entry_result.expect("Failed to read file in given path!");
-
-        if let Ok(file_type) = entry.file_type() {
-            if file_type.is_dir() {
-                dirs.push(entry.path());
-                collect_recurse(entry.path(), dirs);
-            }
-        }
-    }
-}
-
-fn collect_meta_files(path: String) -> Vec<MetaFile> {
-    // First fetch all the directories within a project
-    let mut dirs = Vec::<PathBuf>::new();
-    collect_recurse(path, &mut dirs);
-
-    // Then collect them
-    //println!("Collecting meta files...");
-    let collect_multi = true;
-
-    return if collect_multi {
-        //let drop = dropwatch::Dropwatch::new_begin("META_COLLECT");
-
-        let collector = MetaFileCollector::new(dirs);
-        collector.wait();
-
-        collector.consume()
-    } else {
-        //let drop = dropwatch::Dropwatch::new_begin("META_COLLECT");
-
-        let mut metas = Vec::<MetaFile>::new();
-        for path in dirs {
-            for entry_result in read_dir(path).expect("Failed to read given path!") {
-                // If we can't read a meta file we probably shouldn't be in here
-                let entry = entry_result.expect("Failed to read file in given path!");
-
-                if let Some(extension) = entry.path().extension() {
-                    if extension == "meta" {
-                        let meta = MetaFile::read_from_path(&entry.path()).unwrap();
-
-                        //println!("{:?}", meta);
-                        metas.push(meta);
-                    }
-                }
-            }
-        }
-
-        metas
-    }
-}
 
 fn main() {
     // Handle arguments
@@ -78,6 +26,9 @@ fn main() {
 
             panic!();
         }
+
+        src_assets = args[1].clone();
+        dst_assets = args[2].clone();
     } else {
         // TODO: ASK FOR ARGS!
         panic!("Improper arguments!");
@@ -85,10 +36,20 @@ fn main() {
 
     // Before we export, create the temp folder
     let _ = create_dir("ConversionOutput");
+    let export_path = "./ConversionOutput".to_string();
+
+    //
+    // Collection stage
+    //
 
     // We read two projects worth of hash files
     // Any overlap between the two is eliminated (we assume the asset already exists properly)
     let mut missing_metas = Vec::<MetaFile>::new();
+    let mut remapped_metas = HashMap::<String, MetaFile>::new();
+
+    print!("[Collection Stage]: If this is the first time you've done this since rebooting");
+    print!(" you might have to wait a second or two for the OS to cache files and directories!");
+    println!(" Subsequent runs should be much faster though!");
 
     {
         println!("Collecting source meta files...");
@@ -101,11 +62,20 @@ fn main() {
 
         println!("Determining missing meta files...");
         for src_meta in &src_metas {
+            //println!("{:?}", src_meta);
+
             let mut same_found = false;
 
             for dst_meta in &dst_metas {
-                if src_meta.hash == dst_meta.hash {
+                if src_meta.guid_hash == dst_meta.guid_hash {
                     same_found = true;
+                    break;
+                }
+
+                // Is this the same asset but with a different GUID?
+                if src_meta.base_hash == dst_meta.base_hash {
+                    same_found = true;
+                    remapped_metas.insert(src_meta.guid.clone(), dst_meta.clone());
                     break;
                 }
             }
@@ -114,7 +84,72 @@ fn main() {
                 missing_metas.push(src_meta.clone());
             }
         }
+    }
 
+    //
+    // Conversion stage
+    //
+    println!("[Conversion Stage]");
 
+    let mut convert_queue = Vec::<String>::new();
+
+    for a in 3 .. args.len() {
+        convert_queue.push(args[a].clone());
+    }
+
+    while let Some(convert) = convert_queue.pop() {
+        let prefab_path = Path::new(&convert);
+        let mut prefab_file = File::open(prefab_path).unwrap();
+
+        let mut contents = String::new();
+        let size = prefab_file.read_to_string(&mut contents);
+
+        let mut converted_contents = contents.clone();
+
+        // Find all occurrences of "guid"
+        for indice in contents.match_indices("guid: ") {
+            let guid: String = contents.chars().skip(indice.0 + 6).take(32).collect();
+
+            // Check if this has been remapped
+            if let Some(meta_file) = remapped_metas.get(&guid) {
+                converted_contents.replace_range(indice.0 + 6 .. indice.0 + 6 + 32, &meta_file.guid);
+                continue;
+            }
+
+            // Check if this is in our list of missing ones
+            // If so copy it
+            for missing_meta in &missing_metas {
+                if missing_meta.guid == guid {
+                    // Copy the asset (with and without the meta over)
+                    // If the file doesn't exist already, copy it
+                    let (asset_src_path, meta_src_path) = missing_meta.get_paths();
+                    let (asset_dst_path, meta_dst_path) = missing_meta.get_paths_stem(&export_path);
+
+                    if Path::new(&asset_src_path).exists() && !Path::new(&asset_dst_path).exists() {
+                        copy(&asset_src_path, &asset_dst_path).unwrap();
+                    }
+
+                    if Path::new(&meta_src_path).exists() && !Path::new(&meta_dst_path).exists() {
+                        copy(&meta_src_path, &meta_dst_path).unwrap();
+                    }
+
+                    // If this is a prefab, push it to the list of queued conversions
+                    // If it hasn't been pushed already!
+                    if missing_meta.base_name.ends_with(".prefab") {
+                        if !convert_queue.contains(&asset_src_path) {
+                            println!("Converting referenced prefab {:?}", asset_src_path);
+                            convert_queue.push(asset_src_path.clone());
+                        }
+                    }
+
+                    //println!("MATCH: {} = {:?}", guid, missing_meta)
+                }
+            }
+        }
+
+        let mut converted_path = PathBuf::from(&export_path);
+        converted_path.push(prefab_path.file_name().unwrap());
+
+        write(converted_path, converted_contents).unwrap();
     }
 }
--- src/meta_file/meta_file.rs
+++ src/meta_file/meta_file.rs
@@ -7,25 +7,38 @@ use std::collections::hash_map::DefaultHasher;
 /// Unity meta file (GUID only)
 #[derive(Debug, Default, Clone)]
 pub struct MetaFile {
-    /// Where this meta file is located
-    pub path: String,
+    /// The directory of this meta file
+    pub directory: String,
+
+    /// The base name of this meta file
+    pub base_name: String,
 
     /// Unity's GUID for this asset
     pub guid: String,
 
     /// Hash of the GUID (for faster checking)
-    pub hash: u64
+    pub guid_hash: u64,
+
+    /// Hash of the base name
+    pub base_hash: u64
 }
 
 impl MetaFile {
-    // Reads a meta file, grabs the GUID and returns it
+    /// Reads a meta file, grabs the GUID and returns it
     pub fn read_from_path(path: &PathBuf) -> Option<Self> {
         if let Ok(file) = File::open(path) {
-            let mut hasher = DefaultHasher::new();
             let reader = BufReader::new(file);
 
             let mut meta_file = Self::default();
-            meta_file.path = path.display().to_string();
+
+            meta_file.base_name = path.file_stem().unwrap().to_os_string().into_string().unwrap();
+            meta_file.directory = path.parent().unwrap().display().to_string();
+
+            {
+                let mut hasher = DefaultHasher::new();
+                meta_file.base_name.hash(&mut hasher);
+                meta_file.base_hash = hasher.finish();
+            }
 
             for line in reader.lines() {
                 if let Ok(contents) = line {
@@ -33,8 +46,9 @@ impl MetaFile {
                         meta_file.guid = contents.replace("guid:", "").trim().to_string();
 
                         // Hashing the GUID makes overlap comparison BLAZING FAST :P
+                        let mut hasher = DefaultHasher::new();
                         meta_file.guid.hash(&mut hasher);
-                        meta_file.hash = hasher.finish();
+                        meta_file.guid_hash = hasher.finish();
 
                         break;
                     }
@@ -47,5 +61,23 @@ impl MetaFile {
         }
 
         return None;
+    }
+
+    /// Returns the asset and meta file paths with a new stem
+    pub fn get_paths_stem(&self, stem: &String) -> (String, String) {
+        let mut asset_path = PathBuf::from(stem);
+        asset_path.push(&self.base_name);
+
+        let asset_path_string = asset_path.display().to_string();
+
+        let mut meta_path_string = asset_path_string.clone();
+        meta_path_string.push_str(".meta");
+
+        return (asset_path_string, meta_path_string);
+    }
+
+    /// Returns the asset and meta file paths
+    pub fn get_paths(&self) -> (String, String) {
+        return self.get_paths_stem(&self.directory);
     }
 }
